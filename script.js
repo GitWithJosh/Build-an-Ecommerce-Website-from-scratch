7e3962a06d42374c4fec2f73634b5dd8e23864b8
GitWithJosh <meyer.joshua@gmx.de>
2024-02-07 13:54:20 +0100

    created landing page

---
+++ script.js
@@ -0,0 +1,21 @@
+/*
+This file will contain all the JavaScript code for the project.
+It is used to create the functionality of the website.
+*/
+
+// Function to display the mobile navigation
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+  bar.addEventListener('click', () => {
+    nav.classList.add('active');
+  });
+}
+
+if (close) {
+  close.addEventListener('click', () => {
+    nav.classList.remove('active');
+  });
+}
